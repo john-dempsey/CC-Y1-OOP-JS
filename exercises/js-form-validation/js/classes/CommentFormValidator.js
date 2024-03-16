c40c780e3e630d857e91907220c9c4c31b67aea8
John Dempsey <john.dempsey@iadt.ie>
2024-03-16 13:31:03 +0000

    Adding code to lookup valid categories, experiences and languages

--- exercises/js-form-validation/js/classes/CommentFormValidator.js
+++ exercises/js-form-validation/js/classes/CommentFormValidator.js
@@ -3,10 +3,34 @@ import FormValidator from "./FormValidator.js";
 class CommentFormValidator extends FormValidator {
     constructor(_form) {
         super(_form);
+
+        this.validCategories = [];
+        let categorySelect = this.form.querySelector('select[name="category"]');
+        for (let i = 0; i < categorySelect.options.length; i++) {
+            let option = categorySelect.options[i];
+            if (option.value) {
+                this.validCategories.push(option.value);
+            }
+        }
+
+        this.validExperiences = [];
+        let experienceRadios = this.form.querySelectorAll('input[name="experience"]');
+        for (let i = 0; i < experienceRadios.length; i++) {
+            let radio = experienceRadios[i];
+            this.validExperiences.push(radio.value);
+        }
+
+        this.validLanguages = [];
+        let languageCheckboxes = this.form.querySelectorAll('input[name="languages[]"]');
+        for (let i = 0; i < languageCheckboxes.length; i++) {
+            let checkbox = languageCheckboxes[i];
+            this.validLanguages.push(checkbox.value);
+        }
     }
 
     validate() {
         this.errors = {};
+        
         if (!this.isPresent('name')) {
             this.errors['name'] = 'Name is required';
         }
@@ -14,32 +38,30 @@ class CommentFormValidator extends FormValidator {
             this.errors['name'] = 'Name must be at least 6 characters';
         }
 
-        let validCategories = ["Sport", "Music", "Movies"];
         if (!this.isPresent('category')) {
             this.errors['category'] = 'Category is required';
         }
-        else if (!this.isElement('category', validCategories)) {
+        else if (!this.isElement('category', this.validCategories)) {
             this.errors['category'] = 'Invalid category';
         }
 
-        let validExperiences = ["Novice", "Compotent", "Expert"];
         if (!this.isPresent('experience')) {
             this.errors['experience'] = 'Experience is required';
         }
-        else if (!this.isElement('experience', validExperiences)) {
+        else if (!this.isElement('experience', this.validExperiences)) {
             this.errors['experience'] = 'Invalid experience';
         }
 
-        let validLanguages = ["English", "Irish", "Spanish"];
         if (!this.isPresent('languages[]')) {
             this.errors['languages'] = 'Languages is required';
         }
-        else if (!this.isSubset('languages[]', validLanguages)) {
+        else if (!this.isSubset('languages[]', this.validLanguages)) {
             this.errors['languages'] = 'Invalid language';
         }
         else if (!this.maxLength('languages[]', 2)) {
             this.errors['languages'] = 'Please choose at most two languages';
         }
+
         return Object.keys(this.errors).length === 0;
     }
 }
